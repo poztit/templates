16f578285a07308d95bdd3cfe56c9ab8a9b84588
François Illien <francois@illien.org>
2025-07-10 18:40:09 +0200

    revert root template flake

--- flake.nix
+++ flake.nix
@@ -1,21 +1,10 @@
 {
   description = "A collection of nix flake templates";
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs";
-    utils.url = "github:numtide/flake-utils";
+  outputs = { self }: {
+    templates.article = {
+      path = ./article;
+      description = "Research paper template";
+    };
+    defaultTemplate = self.templates.article;
   };
-  outputs = { self, nixpkgs, utils, ... }: utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = import nixpkgs {
-        inherit system;
-      };
-    in
-    {
-      templates.article = {
-        path = ./article;
-        description = "Research paper template";
-      };
-      defaultTemplate = self.templates.article;
-      formatter = pkgs.nixpkgs-fmt;
-    });
 }
