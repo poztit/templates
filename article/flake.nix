6c06c9bc0349dc588c3469066c6ade597be0fce8
François Illien <francois@illien.org>
2025-07-10 18:03:30 +0200

    update flake of the article template and add a formatter to the root flake

--- article/flake.nix
+++ article/flake.nix
@@ -8,30 +8,61 @@
       pkgs = import nixpkgs {
         inherit system;
       };
+      lib = nixpkgs.lib;
+
+      tex = pkgs.texlive.combine {
+        inherit (pkgs.texlive)
+          scheme-medium
+          pgfplots
+          tikzmark
+          latexmk
+          biblatex;
+      };
+
+      src = lib.cleanSource ./.;
     in
     {
+      formatter = pkgs.nixpkgs-fmt;
+
       devShells.default = pkgs.mkShell {
         name = "research-paper";
         packages = with pkgs; [
-          (texlive.combine { inherit (texlive) scheme-full pgfplots; })
+          (python312.withPackages (ps: with ps; [ pygments ]))
+          tex
+          bibtool
+          bibtex-tidy
+          latexindent
         ];
       };
+
       packages.default = pkgs.stdenv.mkDerivation {
         name = "research-paper";
+        inherit src;
+        buildInputs = [ tex ];
+        buildPhase = ''
+          latexmk -pdf -interaction=nonstopmode article
+        '';
+        installPhase = ''
+          mkdir -p $out
+          cp article.pdf $out/
+        '';
+      };
+
+      packages.diff = pkgs.stdenv.mkDerivation {
+        name = "latex-diff";
+        src = ./.;
         buildInputs = with pkgs; [
-          (texlive.combine { inherit (texlive) scheme-full pgfplots; })
+          tex
+          bibtool
+          git
         ];
         buildPhase = ''
-          	    pdflatex article
-          	    bibtex article
-          	    pdflatex article
-          	    pdflatex article
-          	'';
+          git latexdiff --run-bibtex --main article.tex origin/master
+        '';
         installPhase = ''
-          	    mkdir -p $out
-          	    cp article.pdf $out/
-          	'';
-        src = ./.;
+          mkdir -p $out
+          cp diff.pdf $out/
+        '';
       };
     });
 }
--- flake.nix
+++ flake.nix
@@ -1,10 +1,21 @@
 {
   description = "A collection of nix flake templates";
-  outputs = { self }: {
-    templates.article = {
-      path = ./article;
-      description = "Research paper template";
-    };
-    defaultTemplate = self.templates.article;
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    utils.url = "github:numtide/flake-utils";
   };
+  outputs = { self, nixpkgs, utils, ... }: utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in
+    {
+      templates.article = {
+        path = ./article;
+        description = "Research paper template";
+      };
+      defaultTemplate = self.templates.article;
+      formatter = pkgs.nixpkgs-fmt;
+    });
 }
